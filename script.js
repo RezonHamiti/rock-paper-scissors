560d7c036a85483065319fa91c222bb10b12b4ac
RezonHamiti <rezon.hamiti@gmail.com>
2022-12-13 16:28:21 +0100

    add random computer choice

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const choices = ["rock", "paper", "scissors"]
+
+const game = () => {
+    // play game
+    // play five rounds 
+    // console based 
+}
+
+const playRound = () => {
+    const playerSelection = 
+}
+
+const playerChoice = () => {
+
+}
+
+const computerChoice = () => {
+    return choices[Math.floor(Math.random() * choices.length)]
+}
